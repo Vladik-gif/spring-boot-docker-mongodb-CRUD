ea0a20f524ecf6d2e121cf52e4b0ebde7616dab9
Vladik <v4054741@gmail.com>
2023-04-22 15:55:17 +0300

    add (age, carModel)

--- src/main/java/uk/vladik/springbootmongodb/store/entity/ClientEntity.java
+++ src/main/java/uk/vladik/springbootmongodb/store/entity/ClientEntity.java
@@ -9,6 +9,40 @@ public class ClientEntity {
     @Id
     private String id;
     private String username;
+    private int age;
+    private String carModel;
 
     public ClientEntity() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
 }
